7567e7d74e2949d9c492d059a673674df80468e8
dinislam tebuev <3241533+tebuevd@users.noreply.github.com>
2025-03-05 18:52:59 -0800

    tmux config

--- flake.nix
+++ flake.nix
@@ -224,6 +224,42 @@
               font-thicken = true;
             };
           };
+
+          programs.tmux = {
+            enable = true;
+            clock24 = true;
+            plugins = with pkgs.tmuxPlugins; [
+              {
+                plugin = dracula;
+                extraConfig = ''
+                  set -g @dracula-show-battery false
+                  set -g @dracula-show-powerline true
+                  set -g @dracula-refresh-rate 10
+                  set -g @dracula-plugins "cpu-usage ram-usage"
+                  set -g @dracula-battery-label false
+                  set -g @dracula-show-battery-status true
+                  set -g @dracula-no-battery-label " "
+                '';
+              }
+            ];
+            extraConfig = ''
+              set -g mouse on
+              set -g base-index 1
+              set -g pane-base-index 1
+
+              set -g repeat-time 1000
+
+              # Vim-style pane navigation
+              bind h select-pane -L
+              bind j select-pane -D
+              bind k select-pane -U
+              bind l select-pane -R
+
+              # Vim-style window navigation
+              bind -r C-h select-window -t :-
+              bind -r C-l select-window -t :+
+            '';
+          };
         };
     in
     {
