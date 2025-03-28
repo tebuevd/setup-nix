9779aaed7d4ba13951fa8fee056423194099dacd
dinislam tebuev <3241533+tebuevd@users.noreply.github.com>
2025-03-28 11:51:17 -0700

    more neovim stuff

--- home/programs/nvim.nix
+++ home/programs/nvim.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 
+# NOTES:
+# - To manually format, run `:lua vim.lsp.buf.format()`
+#
+
 {
   programs.nvf = {
     enable = true;
@@ -9,31 +13,67 @@
       vim.theme.style = "dark";
 
       vim.autocomplete.blink-cmp.enable = true;
+      vim.autocomplete.blink-cmp.mappings.close = null;
+      vim.autocomplete.blink-cmp.mappings.complete = null;
+
+      vim.autocomplete.blink-cmp.setupOpts.keymap = {
+        preset = "none";
+
+        "<Up>" = [
+          "select_prev"
+          "fallback"
+        ];
+        "<Down>" = [
+          "select_next"
+          "fallback"
+        ];
+
+        # use Ctrl-e to toggle completion, since Ctrl-Space is used by tmux
+        "<C-e>" = [
+          "show"
+          "hide"
+        ];
+      };
 
       # adds format-on-save among other things
       vim.formatter.conform-nvim.enable = true;
 
-      vim.languages.nix.enable = true;
-      vim.languages.nix.lsp.enable = true;
-      vim.languages.nix.lsp.server = "nil";
-      vim.languages.nix.format.enable = true; # run :lua vim.lsp.buf.format()
-      vim.languages.nix.format.package = pkgs.nixfmt-rfc-style;
-      vim.languages.nix.format.type = "nixfmt";
+      vim.languages = {
+        enableLSP = true;
+        enableFormat = true;
+        enableTreesitter = true;
+        enableExtraDiagnostics = true;
 
-      vim.languages.ts.enable = true;
-      vim.languages.ts.lsp.enable = true;
-      vim.languages.ts.format.enable = true;
+        html.enable = true;
+        css.enable = true;
 
-      vim.languages.python.enable = true;
-      vim.languages.python.lsp.enable = true;
-      vim.languages.python.lsp.server = "basedpyright";
-      vim.languages.python.format.enable = true;
-      # vim.languages.python.format.package = pkgs.ruff;
-      # vim.languages.python.format.type = "ruff";
+        tailwind.enable = true;
+        tailwind.lsp.enable = true;
+
+        nix.enable = true;
+        nix.lsp.enable = true;
+        nix.lsp.server = "nil";
+        nix.format.enable = true;
+        nix.format.package = pkgs.nixfmt-rfc-style;
+        nix.format.type = "nixfmt";
+
+        ts.enable = true;
+        ts.lsp.enable = true;
+        ts.format.enable = true;
+
+        python.enable = true;
+        python.lsp.enable = true;
+        python.lsp.server = "basedpyright";
+        python.format.enable = true;
+        python.format.type = "ruff";
+      };
 
       vim.lsp.mappings.goToDefinition = "gd";
-
       vim.telescope.enable = true;
+
+      vim.luaConfigPost = ''
+        vim.o.scrolloff = 5
+      '';
     };
   };
 }
