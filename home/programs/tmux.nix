308caddcaf486f11e8fc41e3fead4e768b10f456
dinislam tebuev <3241533+tebuevd@users.noreply.github.com>
2025-04-02 15:45:21 -0700

    tmux new windows should open in same directory

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -56,9 +56,10 @@
       bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
       bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
 
-      # open panes in the same directory
+      # new panes/windows open in the same directory
       bind '"' split-window -v -c "#{pane_current_path}"
       bind % split-window -h -c "#{pane_current_path}"
+      bind c new-window -c "#{pane_current_path}"
 
       # set status bar
       set -g status-position top
