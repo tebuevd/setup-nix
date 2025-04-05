836f94f731a61f8443a11d4479f729336c222ad3
dinislam tebuev <3241533+tebuevd@users.noreply.github.com>
2025-04-05 12:56:37 -0700

    update homebrew settings

--- darwin/apps/homebrew.nix
+++ darwin/apps/homebrew.nix
@@ -38,7 +38,7 @@
       "stats"
       "steam"
       "superhuman"
-      "tableplus"
+      # "tableplus" -- i'm using an older version due to having an old license
       "tailscale"
       "visual-studio-code"
       "vlc"
@@ -46,10 +46,13 @@
       "zoom"
     ];
 
-    masApps = {
-      "1Password for Safari" = 1569813296;
-      "Velja" = 1607635845;
-      "Voice Type:Real Time Dictation" = 6736525125;
-    };
+    # mas cli is currently broken on macOS 15.4 which results in these app being reinstalled on every rebuild
+    # therefore i'm disabling this for now
+
+    # masApps = {
+    #   "1Password for Safari" = 1569813296;
+    #   "Velja" = 1607635845;
+    #   "Voice Type:Real Time Dictation" = 6736525125;
+    # };
   };
 }
